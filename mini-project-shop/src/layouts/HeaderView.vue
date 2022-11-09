4fa47fe62f98067c830705a737bcab972002be19
idoasisay <ihannah@kakao.com>
2022-11-09 21:24:15 +0900

    init settings

---
+++ mini-project-shop/src/layouts/HeaderView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
